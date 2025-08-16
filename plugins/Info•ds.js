645c124b493ad027e3e88cf5fd6c23744f9acb0b
EazzyHacks <yhair7200@gmail.com>
2025-08-16 06:39:09 +0100

    Update Info•ds.js

--- plugins/Info•ds.js
+++ plugins/Info•ds.js
@@ -10,7 +10,7 @@ return conn.reply(m.chat, '🚩 *Utiliza este comando directamente en el número
 }
 
 let chatId = m.isGroup ? [m.chat, m.sender] : [m.sender]
-let sessionPath = `./${sessions}/`
+let sessionPath = `./ghostSession`
 
 try {
 
@@ -40,6 +40,6 @@ handler.help = ['ds', 'fixmsgespera']
 handler.tags = ['info']
 handler.command = ['fixmsgespera', 'ds']
 
-handler.register = true
+handler.register = false
 
 export default handler
